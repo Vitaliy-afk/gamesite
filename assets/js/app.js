dba665f66249467c3496987d06c8a7937716c772
Vitaliy <53347040+Vitaliy-afk@users.noreply.github.com>
2021-03-16 15:19:58 +0200

    upd

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,7 +60,7 @@ function initSlickSlider() {
 	jQuery('.slider').slick({
 	infinite: true,
 	speed: 3500,
-	slidesToShow: 3,
+	slidesToShow: 4,
 	slidesToScroll: 1,
 	arrows: true,
 	easing: 'ease',
